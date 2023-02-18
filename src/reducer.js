159210af2a435d917ae8eaa94f565bfa7b75878b
Denis Beresnev <shanthi.bunch@gmail.com>
2023-02-18 21:51:28 +0300

    todoToggled action
    With that in mind, let's add the reducer logic for a couple more cases.
    First, toggling a todo's completed field based on its ID

--- src/reducer.js
+++ src/reducer.js
@@ -44,6 +44,26 @@ export default function appReducer(state = initialState, action) {
                 ],
             };
         }
+        case "todos/todoToggled": {
+            return {
+                // Again copy the entire state object
+                ...state,
+                // This time, we need to make a copy of the old todos array
+                todos: state.todos.map((todo) => {
+                    // If this isn't the todo item we're looking for, leave it alone
+                    if (todo.id !== action.payload) {
+                        return todo;
+                    }
+
+                    // We've found the todo that has to change. Return a copy:
+                    return {
+                        ...todo,
+                        // Flip the completed flag
+                        completed: !todo.completed,
+                    };
+                }),
+            };
+        }
         default:
             // If this reducer doesn't recognize the action type, or doesn't
             // care about this specific action, return the existing state unchanged

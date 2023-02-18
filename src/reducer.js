6104463dbcb2adcb9269cf81f49d9daf9eec055b
Denis Beresnev <shanthi.bunch@gmail.com>
2023-02-18 22:28:40 +0300

    Vanilla combining reducers

---
+++ src/reducer.js
@@ -0,0 +1,12 @@
+import todosReducer from "./features/todos/todosSlice";
+import filtersReducer from "./features/filters/filtersSlice";
+
+export default function rootReducer(state = {}, action) {
+    // always return a new object for the root state
+    return {
+        // the value of `state.todos` is whatever the todos reducer returns
+        todos: todosReducer(state.todos, action),
+        // For both reducers, we only pass in their slice of the state
+        filters: filtersReducer(state.filters, action),
+    };
+}

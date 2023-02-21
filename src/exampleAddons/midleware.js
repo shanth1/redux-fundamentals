08f51713ee5639ba7aff8fcc3d3b474e38e50744
Denis Beresnev <shanthi.bunch@gmail.com>
2023-02-21 15:04:31 +0300

    Writing custom middleware
    exampleMiddleware: The outer function is actually the "middleware" itself.
    It will be called by applyMiddleware, and receives a storeAPI object
    containing the store's {dispatch, getState} functions.
    These are the same dispatch and getState functions that are actually part of the store.
    If you call this dispatch function,
    it will send the action to the start of the middleware pipeline.
    This is only called once.
    
    wrapDispatch: The middle function receives a function called next as its argument.
    This function is actually the next middleware in the pipeline.
    If this middleware is the last one in the sequence, then next is actually the original store.dispatch function instead.
    Calling next(action) passes the action to the next middleware in the pipeline.
    This is also only called once
    
    handleAction: Finally, the inner function receives the current action as its argument,
    and will be called every time an action is dispatched.

--- src/exampleAddons/midleware.js
+++ src/exampleAddons/midleware.js
@@ -1,3 +1,17 @@
+// storeAPI contains getState and dispatch function
+export function exampleMiddleware(storeAPI) {
+    // next is dispatch or newDispatch (from enhancer) function
+    return function wrapDispatch(next) {
+        return function handleAction(action) {
+            // Do anything here: pass the action onwards with next(action),
+            // or restart the pipeline with storeAPI.dispatch(action)
+            // Can also use storeAPI.getState() here
+
+            return next(action);
+        };
+    };
+}
+
 export const print1 = (storeAPI) => (next) => (action) => {
     console.log("1");
     return next(action);
